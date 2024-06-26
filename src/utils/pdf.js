1f5c7ca8ab0c182cd1035c176f9affe62d712be2
Filippo Frater <f.frater@custom.it>
2024-06-26 11:20:09 +0200

    fix: better pdf fonts and formatting

--- src/utils/pdf.js
+++ src/utils/pdf.js
@@ -1,21 +1,39 @@
-const Puppeteer = require('puppeteer');
+const puppeteer = require('puppeteer');
+const path = require('path');
 
 module.exports = async function buildPdf(inputFile, outputFile) {
-  const browser = await Puppeteer.launch();
+  const browser = await puppeteer.launch();
   const page = await browser.newPage();
-  await page.goto(`file://${inputFile}`, {
+
+  // Get the absolute path of the HTML file
+  const absolutePath = path.resolve(inputFile);
+  
+  // Load the HTML file
+  await page.goto(`file://${absolutePath}`, {
     waitUntil: 'networkidle0'
   });
+
+  // Wait for all fonts to be loaded
+  await page.evaluate(async () => {
+    await document.fonts.ready;
+  });
+
+  // Get the dimensions of the content
+  const dimensions = await page.evaluate(() => {
+    return {
+      width: document.documentElement.scrollWidth,
+      height: document.documentElement.scrollHeight,
+    };
+  });
+
+  // Generate the PDF
   await page.pdf({
     path: outputFile,
-    format: 'A4',
-    border: 0,
-    margin: {
-      top: '2.54cm',
-      right: '2.54cm',
-      bottom: '2.54cm',
-      left: '2.54cm',
-    },
+    width: dimensions.width,
+    height: dimensions.height,
+    printBackground: true,
+    preferCSSPageSize: true,
   });
+
   await browser.close();
 };
